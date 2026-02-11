ea02ba215c088adff7d4958e1420fc940b577fac
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-02-11 16:09:22 -0300

    Primer commit dart

---
+++ flutter01.dart
@@ -0,0 +1,9 @@
+void main(){
+  
+  String  myName = 'Seibo';
+  
+  print('Hola $myName'); 
+  print('Hola ${myName.toUpperCase()} '); // Cualquier cosa que no sea solo el nombre de la variable va  entre {} 
+  
+  
+}

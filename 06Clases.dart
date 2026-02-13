56cc0e09d19e2c4a20a19739b37bbabd6a06c11c
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-02-13 19:26:42 -0300

    Clases dart 1

---
+++ 06Clases.dart
@@ -0,0 +1,21 @@
+void main(){
+  final wolverine = new Hero('Logan','Regeneración');
+  print(wolverine);
+  print(wolverine.name);
+  print(wolverine.power);
+
+}
+//Creando una clase 
+class Hero{
+  
+  String name;
+  String power;
+
+  //Constructor largo
+ /* Hero(String pName, String pPower)
+  : name = pName,
+    power = pPower;*/
+
+  //Constructor corto
+  Hero(this.name, this.power);
+}

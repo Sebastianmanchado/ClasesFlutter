f8b012aff2c432e45e6c40c2d959ddf2c2422a36
sebastianmanchado <sebastianmanchado.laboral@gmail.com>
2026-02-13 18:57:35 -0300

    Funciones en dart 1

---
+++ 05Funciones.dart
@@ -0,0 +1,22 @@
+void main(){
+  print(greetEveryone());
+  print('Suma: ${addTwoNumbers(10,20)}');
+  print('Suma lambda; ${addTwoNumbers2(10, 20)}');
+
+}
+
+String greetEveryone() => 'Hola a todos'; //funcion lambda o de flecha 
+
+int addTwoNumbers(int a, int b){
+  return a+b; 
+} // funcion normal 
+
+//convierto funcion normal a lambda 
+int addTwoNumbers2(int a, int b)=> a+b;
+
+
+//Un argumento opcional 
+int addTwoNumbersOpcional(int a, [int b = 0]){ //aca b es opcional y en caso de no existir sera igual a 0
+  return a+b; 
+} // a es obligatiorio, entre [] es opcional  
+
